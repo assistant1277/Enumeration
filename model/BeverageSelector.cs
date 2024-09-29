eaf47eba45b9280981d5075bdb60b5f9a3d3d824
assistant1277 <shivarajghangale2002@gmail.com>
2024-09-29 15:53:16 +0530

    Add project files.

---
+++ model/BeverageSelector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration.model
+{
+    internal class BeverageSelector
+    {
+        //enum represents different types of beverages we can choose from
+        public enum Beverages
+        {
+            Water = 1, //water is the first option labeled as 1
+            Coffee,// and coffee will be label as 2
+            Tea,
+            Soda,
+            Juice,
+            Milk
+        }
+
+        //this is method to get basic information of selected beverage
+        public static void GetBeverageDetails(Beverages beverage)
+        {
+            //show the user what beverage they selected
+            Console.WriteLine($"You selected: {beverage}");
+
+            //gives a description for each beverages using ternary conditions
+            string beverageInfo = (beverage == Beverages.Water)? "water is important in life keeps you energetic": (beverage == Beverages.Coffee)? "coffee is popular drink known for its caffeine": (beverage == Beverages.Tea)? "tea is enjoyed worldwide and famous in india and there are many types of tea like green,black,etc": (beverage == Beverages.Soda)? "soda is carbonated drink which includes various flavours of orange,mango,lime and with sugar ": (beverage == Beverages.Juice)? "juice made from fruits or vegetables and is rich in vitamins,proteins,etc ":"milk is a great source of calcium and can be used to make tea,coffee";
+
+            Console.WriteLine(beverageInfo);
+        }
+
+        //method to select beverage based on user input
+        public static void SelectBeverage()
+        {
+            Console.WriteLine("2) program of beverage selection doing with enumeration concept \n");
+            Console.WriteLine("Please select a beverage by entering a number(1 for water, 2 for coffee, 3 for tea, 4 for soda, 5 for juice, 6 for milk) ");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            //validating means ensure it is within the range of 1-6
+            if (userChoice < 1 || userChoice > 6)
+            {
+                Console.WriteLine("invalid choice select number between 1-6");
+                return;//exit this method
+            }
+
+            //convert user input to the corresponding enum value
+            Beverages selectedBeverage = (Beverages)userChoice;
+
+            //getting and display the beverage info
+            GetBeverageDetails(selectedBeverage);
+        }
+    }
+}
---
+++ model/SeasonSelector.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeration
+{
+    internal class SeasonSelector
+    {
+        
+        //enum represents the different seasons of the year
+        public enum Seasons
+        {
+            Spring=1, //spring is the first season label as 1
+            Summer, // and summer will label as 2
+            Autumn, // 3 for autumn
+            Winter //4 for winter
+        }
+
+        //method provides information about the selected season
+        public static void GetSeasonDetails(Seasons season)
+        {
+            //show the user what season they selected
+            Console.WriteLine($" you selected {season}");
+
+            //gives a description for each season using ternary conditions
+            string seasonInfo = (season == Seasons.Spring) ? "mild temperatures in spring" : (season == Seasons.Summer) ? "hot temperature and vacations period in summer" : (season == Seasons.Autumn) ? "cooler weather autumn is" : "winter is coldest season";
+        
+            Console.WriteLine(seasonInfo );
+        }
+
+
+        //method allows the user to select a season by entering a number
+        public static void SelectSeason()
+        {
+            Console.WriteLine("1) program of season selection doing with enumeration concept \n");
+            Console.WriteLine("select a season by entering number(1 for spring, 2 for summer, 3 for autumn, 4 for winter) ");
+            int userChoice=int.Parse(Console.ReadLine()); //get the user choice and convert it to an integer
+
+            //validating means ensure it is within the range of 1-4
+            if (userChoice<1 || userChoice > 4)
+            {
+                Console.WriteLine("invalid choice select number between 1-4");
+                return; //exit this method
+            }
+
+            //convert user input to corresponding enum value
+            Seasons selectedseason = (Seasons)userChoice;
+
+            //getting and display the season info
+            GetSeasonDetails(selectedseason);
+        }
+
+    }
+}
